362319c3ca30b543e217b333ff3b1dc999a0d070
XaCourse <courses4xa@gmail.com>
2023-05-02 10:14:56 +0530

    add data

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logger
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -21,5 +21,5 @@ if __name__=="__main__":
     try:
         a=1/0
     except Exception as e:
-        logger.info("Divide by zero")
+        logging.info("Divide by zero")
         raise CustomException(e, sys)
